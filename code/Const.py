cfc6b2e41ae8c0f31b95497029f5dc20b5be4258
Edson Kelvin <edsonkelvin19@gmail.com>
2025-03-27 00:01:35 -0300

    Creating player shot Creating enemy shot

--- code/Const.py
+++ code/Const.py
@@ -17,9 +17,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 1,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 2,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
     'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
 
 ENTITY_HEALTH = {
@@ -31,9 +35,20 @@ ENTITY_HEALTH = {
     'Level1Bg5': 999,
     'Level1Bg6': 999,
     'Player1': 300,
+    'Player1Shot': 1,
     'Player2': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
+    'Enemy1Shot': 1,
     'Enemy2': 60,
+    'Enemy2Shot': 1,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 20,
+    'Enemy1': 100,
+    'Enemy2': 200,
 }
 
 # M
@@ -53,7 +68,7 @@ PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
                     'Player2': pygame.K_RIGHT}
 PLAY_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
-                  'Player2': pygame.K_LCTRL}
+                  'Player2': pygame.K_RCTRL}
 
 # S
 SPAWN_TIME = 4000
