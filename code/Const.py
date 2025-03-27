4e3eb0608d9206ee5ad69b5e800e00b512e2a92f
Edson Kelvin <edsonkelvin19@gmail.com>
2025-03-26 22:14:33 -0300

    Creating players and Enemies Moving Players and Enemies Creating Mediator Pattern Enemies destruction

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,13 @@
 # C
+import pygame
+
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 128)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -12,11 +16,25 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1': 3,
+    'Player2': 3,
+    'Enemy1': 2,
+    'Enemy2': 1,
 }
 
-# W
-WIN_WIDTH = 576
-WIN_HEIGHT = 324
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -24,3 +42,22 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_w,
+                 'Player2': pygame.K_UP}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_s,
+                   'Player2': pygame.K_DOWN}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_a,
+                   'Player2': pygame.K_LEFT}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_d,
+                    'Player2': pygame.K_RIGHT}
+PLAY_KEY_SHOOT = {'Player1': pygame.K_LCTRL,
+                  'Player2': pygame.K_LCTRL}
+
+# S
+SPAWN_TIME = 4000
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
